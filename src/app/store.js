5c44e25fe874476f6b10f52a07dc47c0c6ef3efa
mhenrique94 <mhenrique94@outlook.com>
2022-12-18 20:54:27 -0300

    Adds initial function to save a new task

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import toggleReducer from './redux/toggleSlice';
 import todoReducer from './redux/todoSlice'
 export const store = configureStore({
+  devTools: true,
   reducer: {
     toggleMenu: toggleReducer,
     addTodo:todoReducer
--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../app/redux/todoSlice";
 import { TextField, Box, Button } from '@mui/material';
 import { AddCircle } from '@mui/icons-material';
 
-export default function AddTodoForm() {
+const AddTodoForm = () => {
+  const [value, setValue] = useState()
+  const dispatch = useDispatch()
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    dispatch(addTodo({
+      title: value
+    }))
+
+  }
 
   return (
     <>
@@ -13,11 +25,13 @@ export default function AddTodoForm() {
         }}
         noValidate
         autoComplete="off"
+        onSubmit={onSubmit}
       >
         <Box sx={{ display: 'flex' }} style={{ width: '100%' }}>
-          <TextField id="outlined-basic" defaultValue={'Add new task'} variant="outlined" color="primary" required focused sx={{ input: { color: '#504475' }, flexGrow: 1 }}>
+          <TextField id="outlined-basic" placeholder="New task" value={value} variant="outlined" color="primary" required focused sx={{ input: { color: '#504475' }, flexGrow: 1 }}
+        onChange={(event) => setValue(event.target.value)}>
           </TextField>
-          <Button color="primary">
+          <Button color="primary" type="submit">
             <AddCircle />
           </Button>
         </Box>
@@ -25,3 +39,5 @@ export default function AddTodoForm() {
     </>
   )
 }
+
+export default AddTodoForm
